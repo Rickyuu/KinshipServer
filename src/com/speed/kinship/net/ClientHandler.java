5fbb51f8ef72886ff6af29ef61bc128290541c85
rickyuu <rickyliang92@gmail.com>
2014-10-16 17:00:12 +0800

    preliminary design without testing socket

---
+++ src/com/speed/kinship/net/ClientHandler.java
@@ -0,0 +1,68 @@
+package com.speed.kinship.net;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+import com.speed.kinship.model.Identity;
+
+public class ClientHandler implements Runnable {
+
+	private Socket socket;
+	
+	public ClientHandler(Socket socket) {
+		this.socket = socket;
+	}
+	
+	@Override
+	public void run() {
+		try {
+			InputStream inputStream = socket.getInputStream();
+			DataInputStream dataInputStream = new DataInputStream(inputStream);
+			int comeInSize = dataInputStream.readInt();
+			byte[] comeInBytes = new byte[comeInSize];
+			int currentLength = 0;
+			while(currentLength < comeInSize) {
+				currentLength += dataInputStream.read(comeInBytes, currentLength, comeInSize-currentLength);
+			}
+			Object comeInObject = ObjectByteHelper.getInstance().bytesToObj(comeInBytes);
+			MethodMessage methodMessage = (MethodMessage) comeInObject;
+			Object resultObject = distributeMethod(methodMessage);
+			
+			OutputStream outputStream = socket.getOutputStream();
+			DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+			byte[] resultObjectBytes = ObjectByteHelper.getInstance().objToBytes(resultObject);
+			int writeSize = resultObjectBytes.length;
+			dataOutputStream.writeInt(writeSize);
+			dataOutputStream.write(resultObjectBytes);
+			
+			dataInputStream.close();
+			inputStream.close();
+			dataOutputStream.close();
+			outputStream.close();
+			socket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	private Object distributeMethod(MethodMessage methodMessage) {
+		String methodName = methodMessage.getName();
+		if(methodName.equals(Constants.REGISTER)) {
+			Arguments arguments = methodMessage.getArguments();
+			String username = (String) arguments.getArgument("username");
+			String password = (String) arguments.getArgument("password");
+			Identity identity = (Identity) arguments.getArgument("identity");
+			System.out.println(username + " " + password + " " + identity);
+			return true;
+		} else if(methodName.equals(Constants.LOGIN)) {
+			
+		}
+		return null;
+	}
+
+}
---
+++ src/com/speed/kinship/net/ServerListener.java
@@ -0,0 +1,25 @@
+package com.speed.kinship.net;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerListener {
+	
+	public static void main(String args[]) {
+		try {
+			@SuppressWarnings("resource")
+			ServerSocket serverSocket = new ServerSocket(Constants.PORT_NUM);
+			while(true) {
+				Socket clientSocket = serverSocket.accept();
+				Runnable clientHandler = new ClientHandler(clientSocket);
+				Thread thread = new Thread(clientHandler);
+				thread.start();
+				System.out.println("got a connection");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
